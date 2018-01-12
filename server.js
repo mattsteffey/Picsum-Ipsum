666cf37dc1324863dc3e065f9b29d2a65f51e354
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-12 15:29:13 -0700

    fingers crossed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var app     = express();
 var port    = process.env.PORT || 3000;
-var app		= require('./public/app');
 var bodyparser = require('body-parser');
 
 //controller
