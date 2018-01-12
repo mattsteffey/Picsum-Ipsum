3b69cbe510ffaf5ebbd17fe30a47a0bdb4e78fb6
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-11 19:05:42 -0700

    editing facebook functionality

--- public/app.js
+++ public/app.js
@@ -20,6 +20,31 @@ function renderHTML(data) {
 
 output.insertAdjacentHTML('beforeend', htmlString);
 
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '193516051387162',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    	});
+     
+     FB.getLoginStatus(function(response) {
+       if (response.status === 'connected') {
+            FB.api('/me', function(response) {
+    		console.log(JSON.stringify(response));
+			});
+		}
+	});
+};
+
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 }
 
 console.log('front end!');
