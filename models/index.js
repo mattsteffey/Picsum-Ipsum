c7ccb1a2d94e95428e5b60a119ddc76d523e7ae4
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-12 12:29:03 -0700

    save button

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+// var savedSchema = new Schema({
+//  	token: String,
+//    		 savedData: {
+//     		word: String,
+//     		ipsum:  String,
+//    		}
+// });
