8cd81ad5206734adef76d9ae65596aff0286c135
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-15 10:53:43 -0700

    qr DOM

--- public/app.js
+++ public/app.js
@@ -73,6 +73,7 @@ copy.addEventListener('click', function() {
 
 //Changes title text 
 btn.addEventListener('click', function() {
+document.getElementById("wordOutput").style.backgroundImage = "";
 var x = document.getElementById('input');
 var y = document.getElementById('picsum');
 setTimeout(function(){y.innerHTML = "puppies";}, 100);
