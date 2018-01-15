21ada64347e59ca935d1068d32faf4e3b4eeae23
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-15 15:25:26 -0700

    keep going to roll back

--- models/index.js
+++ models/index.js
@@ -1,17 +1,3 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
-
-function pageRender(req, res){
-	res.sendFile(__dirname + '/views/index.html'); //Renders the viewable ((index.html))
-}
-
-function privacyRender(req, res){
-	res.sendFile(__dirname + '/views/privacypolicy.htm'); //Renders the viewable ((privacypolicy.htm))
-}
-
-//route
-app.get('/', pageRender); 
-app.get('/privacy', privacyRender); 
-
-module.exports = router;
