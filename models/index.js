71dfc71e5b996773c7d058f97f48a8e420685d1a
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-15 18:04:34 -0700

    db shit

--- models/index.js
+++ models/index.js
@@ -1,4 +1,9 @@
 var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI ||
+				 process.env.MONGOLAB_URI ||
+				 process.env.MONGOHQ_URL ||
+				 "mongodb://localhost/picsumipsum");
+
 
 
 module.exports.User = require('./users');
