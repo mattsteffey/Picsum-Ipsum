f387422be22e05c06e6f4a27afbf36ff3ff501ff
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-16 15:46:23 -0700

    testing capitalize

--- public/app.js
+++ public/app.js
@@ -46,8 +46,7 @@ function renderHTML(data) {
   // ipsumOutput = htmlString;
   x = htmlString.split(".");
   for (i = 0; i < x.length; i++) {
-  y = [i].charAt(0).toUpperCase() + [i].slice(1);
-  console.log(y);
+  console.log(x[i].charAt(0).toUpperCase() + x[i].slice(1));
   }
 
   output.insertAdjacentHTML('beforeend', htmlString);
