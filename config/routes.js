aafbf67543ce0e9aa29a3b4ba7f7af31ca59a9bd
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-15 18:47:43 -0700

    database structure

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,15 @@
 var express = require("express");
-var router = express.Router();
+var route = express.Router();
 var userControllers = require('../controllers/controllers.js');
 var app = express();
 
 app.route('/', pageRender)
- .get(userControllers.pageRender);
+ .get(userControllers.pageRender)
 	
 app.route('/privacy')
- .get(userControllers.privacyRender);
+ .get(userControllers.privacyRender)
 
 app.route('/api/savedfavorites')
- .post(userControllers.saveFavorite);
+ .post(userControllers.saveFavorite)
 
 module.exports = app;
