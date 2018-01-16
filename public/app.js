bd9ab20511d36a3b99548ededbae2fc387657511
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-16 10:30:06 -0700

    google auth

--- public/app.js
+++ public/app.js
@@ -5,34 +5,14 @@ var tokenId = "";
 var wordInput = "";
 var ipsumOutput = "";
 
-///////////////FACEBOOK DATA///////////////FACEBOOK DATA///////////////FACEBOOK DATA///////////////FACEBOOK DATA
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '193516051387162',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.11'
-    	});
-     
-     FB.getLoginStatus(function(response) {
-       if (response.status === 'connected') {
-            FB.api('/me', function(response) {
-    		tokenId = response.id;
-			});
-		}
-	});
-};
-
-(function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-///////////////FACEBOOK DATA///////////////FACEBOOK DATA///////////////FACEBOOK DATA///////////////FACEBOOK DATA
-
+// GOOGLEDATA
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+//
 
 // Button to fetch API Data from Datamuse
 btn.addEventListener('click', function() {
