bcf05babf10bcb70c4b0689b77e092bdca1aea7f
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-16 18:16:32 -0700

    slicing

--- public/app.js
+++ public/app.js
@@ -40,7 +40,7 @@ function renderHTML(data) {
       for (i = 1; i < x.length; i++) {
       output.insertAdjacentHTML('beforeend', x[i].charAt(1).toUpperCase() + x[i].substr(2)+ ". ");
       }
-  output.value = output.value.slice(0, -3); 
+  output.value = output.value.slice(0, -3) + '.'; 
   
 }
 
