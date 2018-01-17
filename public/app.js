a76305189f4db24765631aacea9c795022a13d52
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-01-16 17:31:36 -0700

    super cool sentences

--- public/app.js
+++ public/app.js
@@ -42,7 +42,7 @@ function renderHTML(data) {
       output.insertAdjacentHTML('beforeend', x[i].charAt(1).toUpperCase() + x[i].substr(2)+ ". ");
       }
 
-  (output.value).slice(0, -2);
+  (wordOutput.value).slice(0, -2);
       
   // output.insertAdjacentHTML('beforeend', htmlString);
 }
